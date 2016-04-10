42f926c7b159ce38fb5acdac468e565c925210dc
t1mmy <jeroen.timmermans@student.uhasselt.be>
2016-04-10 17:59:08 +0200

    Conrollers en model van Activityoverlord 2.0 porject https://github.com/irlnathan/activityoverlord20/tree/master/api/models Basis om front end met AngularJS te maken

---
+++ api/controllers/PageController.js
@@ -0,0 +1,11 @@
+/**
+ * PageController
+ *
+ * @description :: Server-side logic for managing Pages
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
